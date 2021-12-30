8e187904836b5ab4c60672a4f69fba5a07f2647c
Mahmudul2017 <mahmudulhasanshah@gmail.com>
2021-12-30 12:58:30 +0600

    Initial Commit

---
+++ RealmCrazyProgramming-IOS/RealmCrazyProgramming_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmCrazyProgramming_IOSApp.swift
+//  RealmCrazyProgramming-IOS
+//
+//  Created by Jaman on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RealmCrazyProgramming_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
